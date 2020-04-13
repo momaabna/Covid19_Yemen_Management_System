6c01fd3eac3e65639ab29b5a3b3d9f53c5a1ba59
momaabna <momaabna@gmail.com>
2020-04-13 20:36:07 +0300

    add permissions

---
+++ session.php
@@ -0,0 +1,34 @@
+<?php
+   
+if(isset($_COOKIE['cookie'])){
+   $sok = false;
+   $user_check = mysqli_real_escape_string($db,$_COOKIE['username']);
+   $session_check = mysqli_real_escape_string($db,$_COOKIE['cookie']);
+   //$user_check = mysqli_real_escape_string($db,$_SESSION['tocken']); 
+   $result = mysqli_query($db,"SELECT * FROM users WHERE username = '$user_check' and session = '$session_check'");
+   
+   $row = mysqli_fetch_assoc($result);
+    
+    
+    
+    $user_profile_f_name =$row['name'];
+	$user_profile_f_phone= $row['phone'];
+    
+   $count = mysqli_num_rows($result);
+   $login_session = $row['username'];
+   $login_permission =$row['permission'];
+   $user_id = $row['id'];
+   $q_id = $row['q_id'];
+   if(!isset($_COOKIE['cookie'])){
+      header("location:./admin/login.php");
+   }else{
+	   if($count!=1){
+      header("location:./admin/login.php");
+   }else{
+	   $sok = true;
+   }
+   }
+}else{
+    header("location:./admin/login.php");
+}
+?>
